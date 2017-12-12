8a15e1010328f0aec55a4bcf62baf89038f23965
jjjayyy <hwinnovation@naver.com>
2017-12-12 10:02:08 +0900

    테스트

---
+++ temptest.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function(req, res) {
+ res.writeHead(200, {'Content-Type' : 'text/html'});
+ res.write("Hellow Nod1e");
+ res.end();
+}).listen(4000, function() {
+ console.log("server is listening on 88dfd000");
+});
+
